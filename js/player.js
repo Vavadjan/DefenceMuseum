c3c85d8f3b33163abba5bdb1ae5d44411066990b
Vavadjan <vavadjan@gmail.com>
2023-05-24 13:27:45 +0300

    на старт

---
+++ js/player.js
@@ -0,0 +1,19 @@
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '100%',
+        width: '100%',
+        videoId: '4keBAwoHz0E',
+        playerVars: {
+            rel: 0,
+            controls: 0,
+            showinfo: 0
+        }
+    });
+}
